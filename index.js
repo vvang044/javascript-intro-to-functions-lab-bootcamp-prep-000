19bc4d52daca33e5908c5ea5de2c183f712a61d2
Vik Vangala <vvang044@gmail.com>
2019-05-20 17:14:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function sayHiToGrandma(string){
   if(string === string.toLowerCase()){
     return "I can't hear you!"
   } else if (string === string.toUpperCase()){
-    return "YES INDEED"
+    return "YES INDEED!"
   } else if (string === "I love you, Grandma.")
-    return "I love you, too"
+    return "I love you, too."
     
 }
