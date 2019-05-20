add0ec7b4fec15387d8645058f8d308220f9de2c
Vik Vangala <vvang044@gmail.com>
2019-05-20 17:12:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  var lowercase = 'hello!'
   
-  if()
+  
+  if(string === string.toLowerCase()){
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()){
+    return "YES INDEED"
+  } else if (string === "I love you, Grandma.")
+    return "I love  you, Grandma"
+    
 }
